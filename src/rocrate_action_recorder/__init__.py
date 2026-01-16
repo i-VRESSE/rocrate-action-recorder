130c7f5e0d61e1d95f2ce23c3c9ba5eaf76295d4
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2026-01-16 09:26:27 +0100

    Lower to py3.13 so rocraters is available as bin whl

--- src/rocrate_action_recorder/__init__.py
+++ src/rocrate_action_recorder/__init__.py
@@ -1,4 +1,16 @@
 
 
-def record():
+def record(
+    args,
+    inputs,
+    outputs,
+    parser,
+    start_time,
+    crate_dir=None,
+    argv=None,
+    end_time=None,
+    current_user=None,
+    software_version=None,
+    dataset_license=None,
+):
     raise NotImplementedError("This is a placeholder for the record function.")
